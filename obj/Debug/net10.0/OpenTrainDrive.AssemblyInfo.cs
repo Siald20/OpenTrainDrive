6ef96946baf8f8705c6dfe70b89343b03f10c18c
hualder69 <hansueli.alder@batec.net>
2026-01-10 16:32:06 +0000

    css-Dateinamen ebenfalls noch angepasst

--- obj/Debug/net10.0/OpenTrainDrive.AssemblyInfo.cs
+++ obj/Debug/net10.0/OpenTrainDrive.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OpenTrainDrive")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff306b6bb1de615ebfacbddce928c5475ba90429")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b379b830a8aa3ee7afe9b7e739506082964a98a")]
 [assembly: System.Reflection.AssemblyProductAttribute("OpenTrainDrive")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OpenTrainDrive")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
